fe4bf54400ff73c330b9be609a54f66137cad40b
彭 <958953245@qq.com>
2018-11-26 19:53:39 +0800

    11111111111111

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,8 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("java");
+        long l = System.currentTimeMillis();
+        System.out.println(l);
     }
+
 }
